49bbe80df90353a2e8dbf19c98adb714987439a4
Brage <draaangooo@gmail.com>
2023-11-17 13:43:19 +0100

    wip: chess

--- assignment-8/sjakk.py
+++ assignment-8/sjakk.py
@@ -1,4 +1,5 @@
 from typing import Union
+from re import sub as re_sub
 
 X_Str = str  # A, B, .. AA, AB, .. osv.
 Y_Str = int  # 1, 2, 3, .. osv (ikke 0-indeksert)
@@ -27,6 +28,8 @@ PIECE_IDS = {
     "Q": 4,
     "ROOK": 5,
     "R": 5,
+    "PASSANT": 6,
+    "G": 6,  # Ghost
 }
 
 PIECE_STRINGS = {
@@ -37,6 +40,7 @@ PIECE_STRINGS = {
         PIECE_IDS["PAWN"]: "♟",
         PIECE_IDS["QUEEN"]: "♛",
         PIECE_IDS["ROOK"]: "♜",
+        PIECE_IDS["PASSANT"]: "",
     },
     SIDES["WHITE"]: {
         PIECE_IDS["BISHOP"]: "♗",
@@ -45,6 +49,7 @@ PIECE_STRINGS = {
         PIECE_IDS["PAWN"]: "♙",
         PIECE_IDS["QUEEN"]: "♕",
         PIECE_IDS["ROOK"]: "♖",
+        PIECE_IDS["PASSANT"]: "",
     },
 }
 
@@ -257,7 +262,7 @@ class Board:
         Setter en gitt verdi på posisjonen (x, y)
         """
 
-        self.pieces[self.translate(*position)] = piece
+        self.pieces[self.translate(*position)] = piece_or_none
 
     def get(self, x: X, y: Y) -> Union[Any_Piece, None]:
         """
@@ -303,7 +308,7 @@ class Piece:
         return self.string
 
     def _translate_move(self, move: MatrixPosition) -> MatrixPosition:
-        relative_x = self.position[0] + move[0] * self._direction
+        relative_x = self.position[0] + move[0]
         relative_y = self.position[1] + move[1] * self._direction
 
         return relative_x, relative_y
@@ -346,11 +351,11 @@ class Piece:
             piece_at_position = self.board.get(x, current_y)
 
             if piece_at_position == None:
-                positions.add((x, current_y))
+                positions.add((current_x - x, 0))
                 continue
 
             if piece_at_position.side != self.side:
-                positions.add((x, current_y))
+                positions.add((current_x - x, 0))
 
             break
 
@@ -358,11 +363,11 @@ class Piece:
             piece_at_position = self.board.get(x, current_y)
 
             if piece_at_position == None:
-                positions.add((x, current_y))
+                positions.add((current_x - x, 0))
                 continue
 
             if piece_at_position.side != self.side:
-                positions.add((x, current_y))
+                positions.add((current_x - x, 0))
 
             break
 
@@ -383,95 +388,67 @@ class Piece:
             piece_at_position = self.board.get(current_x, y)
 
             if piece_at_position == None:
-                positions.add((current_x, y))
+                positions.add((0, current_y + y * self._direction))
                 continue
 
             if piece_at_position.side != self.side:
-                positions.add((current_x, y))
+                positions.add((0, current_y + y * self._direction))
 
             break
 
         for y in range(*y_down):
             piece_at_position = self.board.get(current_x, y)
 
+            print(current_y + y * self._direction)
+
             if piece_at_position == None:
-                positions.add((current_x, y))
+                positions.add((0, current_y + y * self._direction))
                 continue
 
             if piece_at_position.side != self.side:
-                positions.add((current_x, y))
+                positions.add((0, current_y + y * self._direction))
 
             break
 
         return list(positions)
 
     def get_diagonal_line_of_sight(
-        self, current_x: X, current_y: Y, max_moves: int | None = None
+        self, current_x: X, current_y: Y, max_moves=None
     ) -> list[MatrixPosition]:
         positions: set[MatrixPosition] = set()
 
-        x_start = current_x - max_moves if max_moves != None else 0
-        y_start = current_y - max_moves if max_moves != None else 0
-        x_end = current_x + max_moves if max_moves != None else self.board.width
-        y_end = current_y + max_moves if max_moves != None else self.board.height
+        x_limit = self.board.width
+        y_limit = self.board.height
 
-        x_left = current_x - 1, x_start, -1
-        x_right = current_x + 1, x_end
+        def check_and_add_position(
+            current_position: MatrixPosition, position: MatrixPosition
+        ) -> bool:
+            current_x, current_y = current_position
+            x, y = position
 
-        y_up = current_y - 1, y_start, -1
-        y_down = current_y + 1, y_end
-
-        for x in range(*x_left):
-            for y in range(*y_up):
+            if 0 <= x < x_limit and 0 <= y < y_limit:
                 piece_at_position = self.board.get(x, y)
 
-                if piece_at_position == None:
-                    positions.add((x, y))
-                    continue
+                if piece_at_position is None:
+                    positions.add((x - current_x, (y - current_y) * self._direction))
+                    return True
+                elif piece_at_position.side != self.side:
+                    positions.add((x - current_x, (y - current_y) * self._direction))
 
-                if piece_at_position.side != self.side:
-                    positions.add((x, y))
+            return False
 
-                break
+        for x_dir, y_dir in [(1, 1), (1, -1), (-1, 1), (-1, -1)]:
+            x = current_x
+            y = current_y
 
-        for x in range(*x_left):
-            for y in range(*y_down):
-                piece_at_position = self.board.get(x, y)
+            for _ in range(
+                max_moves if max_moves is not None else max(x_limit, y_limit)
+            ):
+                x += x_dir * self._direction
+                y += y_dir * self._direction
 
-                if piece_at_position == None:
-                    positions.add((x, y))
-                    continue
-
-                if piece_at_position.side != self.side:
-                    positions.add((x, y))
-
-                break
-
-        for x in range(*x_right):
-            for y in range(*y_up):
-                piece_at_position = self.board.get(x, y)
-
-                if piece_at_position == None:
-                    positions.add((x, y))
-                    continue
-
-                if piece_at_position.side != self.side:
-                    positions.add((x, y))
-
-                break
-
-        for x in range(*x_right):
-            for y in range(*y_down):
-                piece_at_position = self.board.get(x, y)
-
-                if piece_at_position == None:
-                    positions.add((x, y))
-                    continue
-
-                if piece_at_position.side != self.side:
-                    positions.add((x, y))
-
-                break
+                if not check_and_add_position((current_x, current_y), (x, y)):
+                    break
 
         return list(positions)
 
@@ -482,8 +459,14 @@ class Piece:
         if self.has_moved == False:
             self.has_moved = True
 
-        board._move(self, new_position)
+        old_position = self.position
+
+        self.board.set(new_position, self)
         self.position = new_position
+
+        if self.piece_id == PIECE_IDS["PAWN"]:
+            if new_position[1] == 0 or new_position[1] == self.board.height - 1:
+                self.promote_to_queen()
 
 
 class Bishop(Piece):
@@ -545,6 +528,8 @@ class Queen(Piece):
             ]
         )
 
+        print(self.get_horizontal_line_of_sight(*self.position))
+
         return self._validate_moves(positions)
 
 
@@ -596,6 +581,20 @@ class Rook(Piece):
         return self._validate_moves(positions)
 
 
+class Passant(Piece):
+    def __init__(
+        self,
+        board: Board,
+        side: Side,
+        position: MatrixPosition,
+        pawn: Pawn,
+        dies_on_half_turn: int,
+    ) -> None:
+        super().__init__(board, PIECE_IDS["PASSANT"], side, position, False)
+        self.dies_on_half_turn = dies_on_half_turn
+        self.parent = pawn
+
+
 PIECE_CLASSES = {
     PIECE_IDS["BISHOP"]: Bishop,
     PIECE_IDS["KING"]: King,
@@ -603,6 +602,7 @@ PIECE_CLASSES = {
     PIECE_IDS["PAWN"]: Pawn,
     PIECE_IDS["QUEEN"]: Queen,
     PIECE_IDS["ROOK"]: Rook,
+    PIECE_IDS["PASSANT"]: Passant,
 }
 
 
@@ -611,9 +611,86 @@ class Game:
         self, forsyth_edwards_notation=DEFAULT_FORSYTH_EDWARDS_NOTATION
     ) -> None:
         self.board = Board()
-        self.turns = []
+        self.turn = 0
 
         self.board.pieces = self.load_pieces(forsyth_edwards_notation)
+
+    def _handle_input(self, input: str) -> str:
+        mutable_input = re_sub(r"\s+", "", input.upper())
+
+        if mutable_input == "quit" or mutable_input == "exit":
+            exit()
+
+        return mutable_input
+
+    def _get_piece_input(self) -> Any_Piece:
+        piece = None
+
+        while piece == None:
+            raw_position_input = self._handle_input(
+                input("Hvilken brikke vil du flytte? ")
+            )
+
+            try:
+                position_input = self.board.parse_position(raw_position_input)
+            except ValueError as e:
+                print(f"Ugyldig input: '{e}'. Vennligst prøv igjen.")
+                continue
+
+            raw_piece = self.board.at(position_input)
+
+            x_str = number_to_letter(position_input[0])
+            position_str = f"{x_str}{position_input[1] + 1}"
+
+            if raw_piece == None:
+                print(
+                    f"Det er ingen brikke i posisjonen {position_str}. Vennligst prøv igjen."
+                )
+                continue
+
+            if raw_piece.side == self.turn % NUMBER_OF_PLAYERS:
+                print(
+                    f"Brikken i posisjonen {position_str} er ikke din. Vennligst prøv igjen."
+                )
+                continue
+
+            piece = raw_piece
+
+        return piece
+
+    def _get_move_input(self, piece: Any_Piece) -> MatrixPosition:
+        available_moves = []
+        move = None
+
+        for available_move in piece.get_moves():
+            available_moves.append(
+                f"{number_to_letter(available_move[0])}{available_move[1] + 1}"
+            )
+
+        print(f"- Du kan flytte til: {', '.join(available_moves)}")
+
+        while move == None:
+            raw_move_input = self._handle_input(
+                input(
+                    f"Hvor vil du flytte {piece} ({number_to_letter(piece.position[0])}{piece.position[1] + 1})?"
+                )
+            )
+
+            try:
+                move_input = self.board.parse_position(raw_move_input)
+            except ValueError as e:
+                print("Ugyldig input: '{e}'. Vennligst prøv igjen.")
+                continue
+
+            if move_input not in piece.get_moves():
+                print(
+                    f"Du kan ikke flytte {piece} til {raw_move_input}. Vennligst prøv igjen."
+                )
+                continue
+
+            move = move_input
+
+        return move
 
     def create_piece_from_forsyth_edwards_notation_key(
         self, piece: str, position: MatrixPosition
@@ -651,12 +728,60 @@ class Game:
         self.turn += 1
 
         clear_console()
-        print(self.board)
+        print(self.board, end="\n\n")
+
+        chosen_piece = self._get_piece_input()
+        original_position = number_to_letter(chosen_piece.position[0]) + str(
+            chosen_piece.position[1] + 1
+        )
+
+        clear_console()
+        print(self.board, end="\n\n")
+
+        chosen_move = self._get_move_input(chosen_piece)
+
+        if chosen_piece.piece_id == PIECE_IDS["PAWN"]:
+            if chosen_move == chosen_piece._translate_move((0, 2)):
+                passant_position = chosen_piece._translate_move((0, 1))
+                passant = Passant(
+                    self.board,
+                    chosen_piece.side,
+                    passant_position,
+                    chosen_piece,
+                    self.turn + 1,
+                )
+
+                self.board.set(passant_position, passant)
+
+        piece_at_chosen_move = self.board.get(*chosen_move)
+
+        if (
+            piece_at_chosen_move != None
+            and piece_at_chosen_move.piece_id == PIECE_IDS["PASSANT"]
+        ):
+            self.board.set(piece_at_chosen_move.parent.position, None)
+
+        old_position = chosen_piece.position
+        chosen_piece.move(chosen_move)
+        self.board.set(old_position, None)
+
+        clear_console()
+        print(self.board, end="\n\n")
+        print(
+            f"You moved {original_position} to {number_to_letter(chosen_move[0])}{chosen_move[1] + 1}"
+        )
+
+        passant_pieces = [
+            piece
+            for piece in self.board.pieces
+            if isinstance(piece, Passant) and piece.dies_on_half_turn == self.turn
+        ]
+
+        for piece in passant_pieces:
+            self.board.set(piece.position, None)
 
 
 game = Game()
 
-print(game.board)
-moves = game.board.at("B2").get_moves()
-for move in moves:
-    print(f"{number_to_letter(move[0])}{move[1] + 1}")
+for half_turn in range(0, 50 * 2):
+    game.next_turn()
